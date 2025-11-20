d0537708da4b60046331dadb2bc0896f1af758db
Petróczki Dávid <petroczki.david@diak.szbi-pg.hu>
2025-11-20 14:20:32 +0100

    Create konyvtar_adatbazis.sql

---
+++ konyvtar_adatbazis.sql
@@ -0,0 +1,23 @@
+CREATE DATABASE konyvtaradatbazis
+     CHARACTER SET utf8mb4
+     COLLATE utf8mb4_hungarian_ci;
+
+USE konyvtaradatbazis;
+
+CREATE TABLE konyvek (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    cim VARCHAR(200) NOT NULL UNIQUE,
+    szerzo VARCHAR(120),
+    kiadasi_ev YEAR, 
+    oldalszam INTEGER CHECK (oldalszam BETWEEN 20 AND 2000),
+    kategoria ENUM('fantasy', 'sci-fi', 'krimi', 'disztópia', 'történelem'),
+    ertekeles FLOAT DEFAULT 5.0
+);
+
+
+CREATE TABLE olvasok (
+    id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    nev NOT NULL VARCHAR(100),
+    szuletesi_datum DATE,
+    varos VARCHAR(50) DEFAULT Budapest
+);
